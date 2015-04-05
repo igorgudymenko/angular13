f2f3bbd7e4fe64c8b366bc541dac2aefb5be62a4
Igor Gudymenko <igorgudymenko@gmail.com>
2015-04-05 23:57:37 +0300

    cards directive

--- src/components/jbCarousel/jbCarousel.directive.js
+++ src/components/jbCarousel/jbCarousel.directive.js
@@ -1,22 +1,57 @@
-angular.module('ajs13').directive('jbCarousel', function() {
+angular.module('ajs13').directive('jbCarousel', function($interval) {
 
   return {
     restrict: 'E',
     scope: {
-      jbItems: '=',
-      jbAction: '&'
+      jbItems: "=",
+      jbAction: "&"
     },
-    transclude: false,
-    templateNamespace: 'html',
-    controller: function($scope, $element, $attrs) {
+    transclude: true,
+    replace: true,
+    template: '<div class="cards" ng-mouseover=stop() ng-mouseleave=start()></div>',
+    compile: function compile (element, attrs, $transcludeFn) {
 
-    },
-    compile: function(element, attrs) {
+      var cards = [];
+      var state = false;
+      var template = angular.element('<div class="card {{card.className}}" ng-repeat="card in jbItems" ng-click="selectCard(card)"></div>');
+      $transcludeFn(
+        element.scope(),
+        function(clone) {
+          template.append(clone);
+        });
+      element.append(template);
+
       return {
-        pre: function preLink(scope, iElement, iAttrs, controller) {
+        pre: function preLink(scope) {
+          scope.selectCard = function(item) {
+            alert(item);
+          };
+          scope.stop = function() {
+            state = true;
+          };
+          scope.start = function() {
+            state = false;
+          };
 
+          for (var i = 0; i < scope.jbItems.length; i++) {
+            var className = "card-" + i;
+            scope.jbItems[i]["className"] = className;
+            cards.push(className);
+          }
         },
-        post: function postLink(scope, iElement, iAttrs, controller) {
+
+        post: function postLink(scope) {
+          function updateTime() {
+            if (!state) {
+              cards.push(cards.shift());
+
+              for (var t = 0; t < scope.jbItems.length; t++) {
+                scope.jbItems[t]["className"] = cards[t];
+              }
+            }
+          }
+          $interval(updateTime, 4000);
+
 
         }
       }
