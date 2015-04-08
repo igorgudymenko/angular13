efa1691d9152b2ee7ae95c61d95bd91b8badf48e
Igor Gudymenko <igorgudymenko@gmail.com>
2015-04-08 23:10:54 +0300

    mask for input

--- src/components/magicInput/magicInput.js
+++ src/components/magicInput/magicInput.js
@@ -24,31 +24,25 @@ angular.module('ajs13').directive('jbMask', function() {
           }
           if (value[i].match(/[0-9]/) && mask[i] != '9') {
             return false;
-          } else if (value[i].match(/[A-z]/) && mask[i] != 'a') {
-            return false;
-          }
+          } else return !(value[i].match(/[A-z]/) && mask[i] != 'a');
         }
-        return true;
       }
-
 
       ngModel.$parsers.push(function(value){
         if (isValid(value, mask)) {
           return value;
-        } else if (value && value.length > 0 && !isValid(value, mask.substr(0, value.length))) {
+        } else if (!isValid(value, mask.substr(0, value.length))) {
           $element.val(undefined);
           return undefined;
         }
       });
 
       ngModel.$formatters.push(function(value) {
-        if (value) {
           if(isValid(value, mask)) {
             return value;
           } else {
             return undefined;
           }
-        }
       });
     }
   }
